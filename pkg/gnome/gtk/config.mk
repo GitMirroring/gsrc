1d2d3e9075d6b50609cf618afd963f2ee64ced7e
carl <carlhansen1234@gmail.com>
2025-11-02 15:08:05 -0800

    modified:   pkg/gnome/foundry/Makefile 	modified:   pkg/gnome/foundry/sha256sums 	modified:   pkg/gnome/gjs/Makefile 	modified:   pkg/gnome/gtk/Makefile 	modified:   pkg/gnome/gtk/config.mk 	modified:   pkg/gnome/gtk/sha256sums 	modified:   pkg/gnu/dominion/Makefile 	modified:   pkg/gnu/gcc15snapshot/Makefile 	modified:   pkg/gnu/gcc15snapshot/sha256sums 	modified:   pkg/gnu/gretl/Makefile 	modified:   pkg/gnu/ncurses/Makefile 	modified:   pkg/gnu/ncurses/sha256sums 	modified:   pkg/gnu/unifont/Makefile 	modified:   pkg/gnu/unifont/sha256sums 	modified:   pkg/other/lean4/Makefile 	modified:   pkg/other/lean4/sha256sums 	modified:   util/gcc15fixer

--- pkg/gnome/gtk/config.mk
+++ pkg/gnome/gtk/config.mk
@@ -5,6 +5,7 @@ CONFIGURE_OPTS ?=
 # what is right?
 #BUILD_OPTS ?=  CXXFLAGS=-Wno-error=array-bounds CFLAGS=-Wno-error=array-bounds
 BUILD_OPTS ?=  CXXFLAGS=-Wwarning=array-bounds CFLAGS=-Wwarning=array-bounds
+#BUILD_OPTS ?=  
 
 #Werror=array-bounds]
 
