eae7d98e36d15f0012708bb04cbd4f666f88d2bf
carl <carlhansen1234@gmail.com>
2025-11-22 16:46:14 -0800

    Changes to be committed: 	modified:   apl/Makefile 	modified:   apl/config.mk 	modified:   binutils/Makefile 	modified:   challenger/Makefile 	modified:   challenger/sha256sums 	new file:   donau/Makefile 	new file:   donau/config.mk 	new file:   donau/sha256sums 	modified:   libeufin/Makefile 	modified:   libeufin/sha256sums 	modified:   taler-exchange/Makefile 	modified:   taler-exchange/sha256sums 	modified:   taler-merchant/Makefile 	modified:   taler-merchant/sha256sums 	modified:   taler-wallet/Makefile 	modified:   unrtf/Makefile

--- pkg/gnu/apl/config.mk
+++ pkg/gnu/apl/config.mk
@@ -1,6 +1,10 @@
 ## Configuration options for apl ##
 
-CONFIGURE_OPTS ?=  LDFLAGS=-L$(prefix)/lib64 
+#CONFIGURE_OPTS ?=  LDFLAGS=-L$(prefix)/lib64 
 #BUILD_OPTS ?= CFLAGS+=-Wno-error=int-in-bool-context
-BUILD_OPTS ?= CFLAGS+="-Wno-error=maybe-uninitialized -Wno-error=class-memaccess" LDFLAGS=-L$(prefix)/lib64 CPPFLAGS+="-Wno-error=maybe-uninitialized -Wno-error=class-memaccess" 
+#BUILD_OPTS ?= CFLAGS+="-Wno-error=maybe-uninitialized -Wno-error=class-memaccess" LDFLAGS=-L$(prefix)/lib64 CPPFLAGS+="-Wno-error=maybe-uninitialized -Wno-error=class-memaccess" 
+CONFIGURE_OPTS ?=
+BUILD_OPTS ?=
+
+
 
