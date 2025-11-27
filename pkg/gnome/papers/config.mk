eb9dd0fa0839a037dd97161e419a05169c42c81b
carl <carlhansen1234@gmail.com>
2025-11-27 13:05:56 -0800

    dap

--- pkg/gnome/papers/config.mk
+++ pkg/gnome/papers/config.mk
@@ -2,7 +2,8 @@
 ## adjust as needed for your local system
 
 CONFIGURE_OPTS ?=
-BUILD_OPTS ?=  CXXFLAGS=-Wwarning=array-bounds CFLAGS=-Wwarning=array-bounds
+#BUILD_OPTS ?=  CXXFLAGS=-Wwarning=array-bounds CFLAGS=-Wwarning=array-bounds
+BUILD_ORTS ?=
 
 
 
