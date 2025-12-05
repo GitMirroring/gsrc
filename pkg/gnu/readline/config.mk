a19018fbb8dbad586bdc39dc49c77fb3e57fee06
carl <carlhansen1234@gmail.com>
2025-12-05 13:40:28 -0800

    modified:   config.mk 	modified:   sha256sums

--- pkg/gnu/readline/config.mk
+++ pkg/gnu/readline/config.mk
@@ -1,6 +1,6 @@
 ## Configuration options for readline ##
 
-CONFIGURE_OPTS ?= --with-curses=yes
+#CONFIGURE_OPTS ?= --with-curses=yes
 #CONFIGURE_OPTS ?= --with-curses=no
+CONFIGURE_OPTS ?=
 BUILD_OPTS ?=
-#CFLAGS=-Wimplicit-function-declaration
