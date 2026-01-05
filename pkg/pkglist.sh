97de2197209a2719a20d8de836c98838b211195d
Carl Hansen <carlhansen@gnu.org>
2026-01-04 19:14:26 -0800

    too tired to write a message, letting "ai" do it

--- pkg/pkglist.sh
+++ pkg/pkglist.sh
@@ -1,4 +1,3 @@
 
-# cd $gsrc/pkg && find . -maxdepth 2 -mindepth 2|grep -v local| grep -v decommis |grep -v external|grep -v tofix |grep -v notinuse  |grep -v gcc | grep -v glibc|sort  >pkglist
-#
-cd /home/carl/build/gsrc/pkg && find . -maxdepth 2 -mindepth 2|grep -v local| grep -v decommis |grep -v external|grep -v tofix |grep -v notinuse |sort  >pkglist
+
+ cd $gsrc/pkg && find . -maxdepth 2 -mindepth 2|grep -v local| grep -v decommis |grep -v external|grep -v tofix |grep -v notinuse  |grep -v gcc | grep -v glibc|sort  >pkglist
