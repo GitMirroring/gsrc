cdc97a8c6594467ed5094542d75fdfe6ded85ef8
Carl Hansen <carlhansen@gnu.org>
2026-01-17 16:31:19 -0800

    too tired to write a message, letting "ai" do it

--- pkg/gnome/gtk/config.mk
+++ pkg/gnome/gtk/config.mk
@@ -2,12 +2,8 @@
 ## adjust as needed for your local system
 
 CONFIGURE_OPTS ?=
-# what is right?
-#BUILD_OPTS ?=  CXXFLAGS=-Wno-error=array-bounds CFLAGS=-Wno-error=array-bounds
-BUILD_OPTS ?=  CXXFLAGS=-Wwarning=array-bounds CFLAGS=-Wwarning=array-bounds
-#BUILD_OPTS ?=  
+BUILD_OPTS ?=  
 
-#Werror=array-bounds]
 
 
 
