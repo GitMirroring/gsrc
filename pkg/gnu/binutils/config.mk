875bfceebc930ef574738e6353c8b43735f93c2d
Carl Hansen <carlhansen@gnu.org>
2026-02-11 16:47:43 -0800

    too tired to write a message, letting "ai" do it

--- pkg/gnu/binutils/config.mk
+++ pkg/gnu/binutils/config.mk
@@ -1,5 +1,4 @@
 ## Configuration options for binutils ##
 
 CONFIGURE_OPTS ?= 
-#--with-sysroot=$(prefix)  --enable-gold
 BUILD_OPTS ?= 
--- pkg/gnu/nettle/config.mk
+++ pkg/gnu/nettle/config.mk
@@ -1,4 +1,5 @@
 ## Configuration options for nettle ##
 
-CONFIGURE_OPTS ?= --disable-openssl --enable-shared
+CONFIGURE_OPTS ?=
+#--disable-openssl --enable-shared
 BUILD_OPTS ?=
