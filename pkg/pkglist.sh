1bdb062b910c83cee5813ffcb6cb8ee11aa55fd6
Carl Hansen <carlhansen@gnu.org>
2026-03-11 11:52:34 -0700

    too tired to write a message, letting "ai" do it

--- pkg/pkglist.sh
+++ pkg/pkglist.sh
@@ -1,3 +1,6 @@
 
 
+
  cd $gsrc/pkg && find . -maxdepth 2 -mindepth 2|grep -v local| grep -v decommis |grep -v external|grep -v tofix |grep -v notinuse  |grep -v gcc | grep -v glibc|sort  >pkglist
+
+# adjust for your purposes
