0679f66ba8e0f171811072bb212e682c6464683f
Carl Hansen <carlhansen@gnu.org>
2026-03-31 13:41:47 -0700

    too tired to write a message, letting "ai" do it

---
+++ pkg/other/root/deps.sh
@@ -0,0 +1,23 @@
+
+
+# see https://root.cern.ch/install/dependencies/
+
+# for debian:
+#Installing required packages with a one-liner:
+
+sudo apt install binutils cmake dpkg-dev g++ gcc libssl-dev git libx11-dev \
+libxext-dev libxft-dev libxpm-dev python3 libtbb-dev libvdt-dev libgif-dev
+
+#Installing most common optional packages with a one-liner:
+
+sudo apt install gfortran libpcre3-dev \
+libglu1-mesa-dev libglew-dev libftgl-dev \
+libfftw3-dev libcfitsio-dev libgraphviz-dev \
+libavahi-compat-libdnssd-dev libldap2-dev \
+ python3-dev python3-numpy libxml2-dev libkrb5-dev \
+libgsl-dev qtwebengine5-dev nlohmann-json3-dev libmysqlclient-dev \
+libgl2ps-dev \
+liblzma-dev libxxhash-dev liblz4-dev libzstd-dev libcurl4-openssl-dev
+
+
+
