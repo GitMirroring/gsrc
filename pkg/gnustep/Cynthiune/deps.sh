60f369323794f6a7f560ead90f8551ba23a22f43
Carl Hansen <carlhansen@gnu.org>
2026-04-02 18:49:43 -0700

    too tired to write a message, letting "ai" do it

---
+++ pkg/gnustep/Cynthiune/deps.sh
@@ -0,0 +1,10 @@
+
+
+# debian
+	sudo apt-get install libid3-tools libid3tag0 libid3tag0-dev \
+  modplug-tools  libmodplug-dev  libmodplug1 \
+  audiofile-tools libaudiofile-dev  libaudiofile1 \
+   libavifile-\*    musepack-tools  
+
+  # probably others
+
