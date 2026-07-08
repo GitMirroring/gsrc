e68cc9d6c434af7436ea36a1fb44f7ac882dbc12
Carl Hansen <carlhansen@gnu.org>
2026-07-07 23:45:58 -0700

    too tired to write a message, letting "ai" do it

--- pkg/other/libgit2/config.mk
+++ pkg/other/libgit2/config.mk
@@ -1,6 +1,8 @@
 ## Configuration options for libgit2 ##
 
 # needed by gnome-builder
-CONFIGURE_OPTS ?=      -DTHREADSAFE=ON -DUSE_SSH=1
-BUILD_OPTS ?=  CFLAGS="-DTHREADSAFE=ON -DUSE_SSH=1"
+CONFIGURE_OPTS ?=
+#-DTHREADSAFE=ON -DUSE_SSH=1
+BUILD_OPTS ?=
+#CFLAGS="-DTHREADSAFE=ON -DUSE_SSH=1"
 
