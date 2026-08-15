2ae56d7e8dda520cb47521c245607812d4998834
Carl Hansen <carlhansen@gnu.org>
2026-08-15 03:24:19 -0700

    too tired to write a message

--- pkg/pkglist.sh
+++ pkg/pkglist.sh
@@ -1,5 +1,6 @@
 
 
+gsrc=/home/carl/build/gsrc
 
  cd $gsrc/pkg && find . -maxdepth 2 -mindepth 2|grep -v local| grep -v decommis |grep -v external|grep -v tofix |grep -v notinuse  |grep -v gcc | grep -v glibc|sort  >pkglist
 
