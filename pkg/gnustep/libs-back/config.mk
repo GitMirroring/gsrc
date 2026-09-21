433e57bb2d41318f0691f2335db4dfce7dea4385
Carl Hansen <carlhansen@gnu.org>
2026-09-20 20:52:18 -0700

    too tired to write a message

--- pkg/gnustep/libs-back/config.mk
+++
@@ -1,4 +0,0 @@
-## Configuration options for gnustep libs-back ##
-
-CONFIGURE_OPTS ?=
-BUILD_OPTS ?=
--- pkg/gnustep/libs-gui/config.mk
+++
@@ -1,4 +0,0 @@
-## Configuration options for gnustep libs-gui ##
-
-CONFIGURE_OPTS ?= 
-BUILD_OPTS ?=
